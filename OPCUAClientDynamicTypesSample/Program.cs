ac8f870d393b37ddb262c93120a5b6440a47c394
Dustin Baron <dustin.baron@aitag.com>
2018-10-10 07:33:28 +0200

    Initialized Solution

---
+++ OPCUAClientDynamicTypesSample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OPCUAClientDynamicTypesSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
